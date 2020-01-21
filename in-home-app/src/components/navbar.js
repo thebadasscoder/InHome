11cc550c60a7fd669d42ba9af720cb82e5b6da41
twilsonpierce <twilsonpierce@gmail.com>
2020-01-21 00:36:43 -0500

    made more changes

--- in-home-app/src/components/navbar.js
+++ in-home-app/src/components/navbar.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import '../stylesheets/components/_navbar.scss';
 
-
 class Navbar extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			username: '',
+			password :''
+		};
+
+		this.handleUsernameChange = this.handleUsernameChange.bind(this);
+		this.handlePasswordChange = this.handlePasswordChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleUsernameChange(e){
+		this.setState({username: e.target.value});
+	}
+
+	handlePasswordChange(e){
+		this.setState({password: e.target.value});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+	}
+
 	render () {
 		return (
 			<div className="in-home-navbar-page">
@@ -13,6 +36,14 @@ class Navbar extends Component {
 					<a href="#" className="forgot-link">Forgot</a> 
 					<span className="navbar-arrow"></span>
 				</nav> 
+
+				<div className="sign-in-form-container"> 
+		      <form className="in-home-form" onSubmit={this.handleSubmit}> 
+		        <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange}/>
+		        <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}/>
+		        <input type="submit" value="Sign in"/>
+		      </form> 
+		    </div>  
 			</div> 
 		)
 	}
